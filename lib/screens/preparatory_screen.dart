2ccc7378fbadc75ec06a5d02c791ddd3eb194264
MelvinForJESUS <melvinthepilgrim@gmail.com>
2025-02-18 19:15:24 +0530

    God is Love

--- lib/screens/preparatory_screen.dart
+++ lib/screens/preparatory_screen.dart
@@ -1,5 +1,6 @@
 // preparatory_screen.dart !
 // ignore_for_file: library_private_types_in_public_api, use_build_context_synchronously, deprecated_member_use
+// God is Love
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
